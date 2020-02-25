52219f246d70f24fa14e06586ff7a5811cf13f93
Leo Dimacuha <leo.dimacuha@gmail.com>
2020-02-25 15:30:32 +1000

    tweaks

--- src/containers/JobsAPITester/JobsAPITester.js
+++ src/containers/JobsAPITester/JobsAPITester.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
@@ -19,23 +19,28 @@ const JobsAPITester = props => {
   const [apiEndPoint, setApiEndPoint] = useState(domain[selectedCountry] + params);
   const [jobs, setJobs] = useState([]);
   const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [dirty, setDirty] = useState(false);
 
   const validateClickHandler = () => {
+    setLoading(true);
+    setDirty(true);
     axios.get(domain[selectedCountry], {
       'headers': {
         'Content-Type': 'application/json',
         'X-api-authenticate': apiKey
       }
     }).then((result) => {
+      setLoading(false);
       setError(result.data.jobs.length > 0 ? '' : 'There are no published jobs in the account.');
       setJobs(sortObject(result.data.jobs, 'title', 'asc'));
     }).catch((error) => {
+      setLoading(false);
       setError(error.response.data);
       setJobs([]);
       return Promise.reject(error);
     });
   }
-
 
   const apiEndPointHandler = (country) => {
     setSelectedCountry(country);
@@ -46,8 +51,12 @@ const JobsAPITester = props => {
     return <li key={job.reference}><a href={job.public_url} target="_blank" rel="noreferrer noopener">{job.title}</a> {job.reference}</li>
   });
 
+  const changeEndPoint = (e) => {
+    setApiEndPoint(e.target.value);
+  }
+
   return (
-    <Fragment>
+    <>
       <label htmlFor="">API End Point</label>
       <InputGroup className="mb-3">
         <DropdownButton
@@ -58,17 +67,18 @@ const JobsAPITester = props => {
           <Dropdown.Item href="#" onClick={() => { apiEndPointHandler("australia"); setApiKey(''); setJobs([]); setError('') }}>Australia</Dropdown.Item>
           <Dropdown.Item href="#" onClick={() => { apiEndPointHandler("canada"); setApiKey(''); setJobs([]); setError('') }}>Canada</Dropdown.Item>
         </DropdownButton>
-        <FormControl aria-describedby="" value={apiEndPoint} onChange={() => { }} />
+        <FormControl aria-describedby="" value={apiEndPoint} onChange={(e) => changeEndPoint(e)} />
       </InputGroup>
 
       <label htmlFor="">API Key</label>
       <InputGroup className="mb-3">
         <FormControl aria-describedby="" value={apiKey} onChange={(e) => { setApiKey(e.target.value); setJobs([]); setError(''); }} />
         <InputGroup.Append>
-          <Button variant="primary" onClick={() => validateClickHandler()}>Validate</Button>
+          <Button variant="primary" className={loading ? 'disabled' : ''} onClick={() => validateClickHandler()}>Validate</Button>
         </InputGroup.Append>
       </InputGroup>
 
+      {dirty && !loading ? jobs.length + ' Jobs' : ''}
       <ul>
         {joblist}
       </ul>
@@ -77,7 +87,7 @@ const JobsAPITester = props => {
           {error}
         </Alert>
         : null}
-    </Fragment>
+    </>
   )
 }
 
