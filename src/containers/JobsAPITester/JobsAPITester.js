72c16a6fd018f4c3352c28cf6ea6d8fb0cb6a13a
Leo Dimacuha <leo.dimacuha@gmail.com>
2021-06-14 10:27:05 +1000

    add params and documentation help text

--- src/containers/JobsAPITester/JobsAPITester.js
+++ src/containers/JobsAPITester/JobsAPITester.js
@@ -2,21 +2,22 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
+import FormGroup from 'react-bootstrap/FormGroup';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Alert from 'react-bootstrap/Alert';
 import FormControl from 'react-bootstrap/FormControl';
 import axios from 'axios';
 import sortObject from '../../helpers/sortObject';
 
+const domain = {
+  'australia': 'https://applynow.net.au/api/v2/jobs?current=true&recursive=true',
+  'canada': 'https://scouterecruit.net/api/v2/jobs?current=true&recursive=true'
+}
+
 const JobsAPITester = props => {
   const [apiKey, setApiKey] = useState('');
   const [selectedCountry, setSelectedCountry] = useState('australia');
-  const [domain, setDomain] = useState({
-    'australia': 'https://applynow.net.au/api/v2/jobs',
-    'canada': 'https://scouterecruit.net/api/v2/jobs'
-  });
-  const [params, setParams] = useState('');
-  const [apiEndPoint, setApiEndPoint] = useState(domain[selectedCountry] + params);
+  const [apiEndPoint, setApiEndPoint] = useState(domain[selectedCountry]);
   const [jobs, setJobs] = useState([]);
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
@@ -44,11 +45,13 @@ const JobsAPITester = props => {
 
   const apiEndPointHandler = (country) => {
     setSelectedCountry(country);
-    setApiEndPoint(domain[country] + params);
+    setApiEndPoint(domain[country]);
+    setJobs([]);
+    setError('');
+    setDirty(false);
   }
 
   const joblist = jobs.map((job) => {
-    console.log(job);
     return <li key={job.reference}>
       <a href={job.public_url} target="_blank" rel="noreferrer noopener">{job.title}</a> {job.reference}
       <br />Entity: {job.entity}
@@ -63,21 +66,26 @@ const JobsAPITester = props => {
   return (
     <>
       <label htmlFor="">API End Point</label>
-      <InputGroup className="mb-3">
-        <DropdownButton
-          as={InputGroup.Prepend}
-          variant="outline-secondary"
-          title={selectedCountry}
-        >
-          <Dropdown.Item href="#" onClick={() => { apiEndPointHandler("australia"); setApiKey(''); setJobs([]); setError('') }}>Australia</Dropdown.Item>
-          <Dropdown.Item href="#" onClick={() => { apiEndPointHandler("canada"); setApiKey(''); setJobs([]); setError('') }}>Canada</Dropdown.Item>
-        </DropdownButton>
-        <FormControl aria-describedby="" value={apiEndPoint} onChange={(e) => changeEndPoint(e)} />
-      </InputGroup>
-
+      <FormGroup>
+        <InputGroup>
+          <DropdownButton
+            as={InputGroup.Prepend}
+            variant="outline-secondary"
+            title={selectedCountry}
+          >
+            <Dropdown.Item href="#" onClick={() => apiEndPointHandler("australia") }>Australia</Dropdown.Item>
+            <Dropdown.Item href="#" onClick={() => apiEndPointHandler("canada") }>Canada</Dropdown.Item>
+          </DropdownButton>
+          <FormControl aria-describedby="" value={apiEndPoint} onChange={(e) => changeEndPoint(e)} />
+        </InputGroup>
+        <small className="form-text text-muted">
+          See API documentation here: <a href="https://scoutv2.docs.apiary.io/#reference/jobs/job-collection/list-jobs" target="_blank">https://scoutv2.docs.apiary.io/</a>
+        </small>
+      </FormGroup>
+  
       <label htmlFor="">API Key</label>
       <InputGroup className="mb-3">
-        <FormControl aria-describedby="" value={apiKey} onChange={(e) => { setApiKey(e.target.value); setJobs([]); setError(''); }} />
+        <FormControl aria-describedby="" value={apiKey} onChange={(e) => { setApiKey(e.target.value); setJobs([]); setError(''); setDirty(false); }} />
         <InputGroup.Append>
           <Button variant="primary" className={apiKey === '' || loading ? 'disabled' : ''} onClick={() => validateClickHandler()}>Validate</Button>
         </InputGroup.Append>
